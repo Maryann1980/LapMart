5868a3368f21f5465367e5ab673e316838efc2fa
Maryann1980 <igwemaryanneifunanya@gmail.com>
2025-03-13 00:18:38 +0100

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Footer.css"; // Import the CSS file
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+      <p>&copy;{new Date().getFullYear()} LapMart. All rights reserved.</p>
+        <nav>
+          <Link to="/">Shop</Link>
+            <Link to="/cart">Cart</Link>
+          <a href="www.linkedin.com/in/maryann-ifunanya-igwe-94083331b">Linkedin</a>
+          <a href="https://github.com/Maryann1980">GitHub</a>
+        </nav>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+import {ShoppingCart} from "@phosphor-icons/react";
+import "./NavBar.css";
+
+export const NavBar = () => {
+    return (
+        <div className="navbar">
+    <h1 className="logo">LapMart</h1>
+    <div className="links">
+        <Link to="/">Shop</Link>
+        <Link to="/cart">
+            <ShoppingCart size={32} />
+        </Link>
+    </div>
+</div>
+
+    );
+
+}
