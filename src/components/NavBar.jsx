7158482177ea5cb3b940003f6a31059cfd6c6607
Maryann1980 <igwemaryanneifunanya@gmail.com>
2025-03-13 00:36:42 +0100

    Fixed missing NavBar.css issue

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {Link} from 'react-router-dom';
 import {ShoppingCart} from "@phosphor-icons/react";
-import "./NavBar.css";
+import "./navbar.css";
 
 export const NavBar = () => {
     return (
